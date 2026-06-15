8d8c5e38e8d1ad6156dadbd567df286b812a9ee8
sbhoomika069-png <sbhoomika069@gmail.com>
2026-06-15 17:02:21 +0530

    panda1

--- panda1.py
+++ panda1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data = {
-    'name': ['harshi','isha','bhoomika','harshika'],
+    'Name': ['harshi','isha','bhoomika','harshika'],
     'age':  [22, 21, 23, 21],
     'Marks': [85, 55, 66,59],
     'city': ['Bhopal', 'Indore', 'Betul', 'sagar'],
@@ -13,3 +13,26 @@ print(df.shape)
 print(df.head(3))
 print(df.dtypes)
 print(df.describe())
+
+#select coloums
+print("df['Name']: \n", df['Name'])
+print(df[['Name', 'Marks']])
+
+## Filter rows
+print(df[df['Marks'] >= 85])
+print(df[df['city'] == 'Bhopal'])
+print( df[ (df['Marks']>=80) & (df['city'] == 'Indore') ] )
+
+def get_grade(x):
+    if x >=90:
+        return 'A'
+    elif x >= 75:
+        return 'B'
+    else:
+        return 'c'
+df['Grade'] = df['Marks'].apply(get_grade)
+print(df['Grade'])
+print('---------------')
+print(df)
+
+
