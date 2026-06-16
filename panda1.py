7f1115195a4f8a39e3141ada524996ced2a18b8b
sbhoomika069-png <sbhoomika069@gmail.com>
2026-06-16 20:02:32 +0530

    seaborn

--- panda1.py
+++ panda1.py
@@ -59,7 +59,7 @@ df2.to_csv('clean_output.csv', index=False) #save'''
 import matplotlib .pyplot as plt
 import numpy as np
 
-months = ['jan', 'Feb', 'Mar', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+'''months = ['jan', 'Feb', 'Mar', 'Apr', 'May', 'jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 sales = [45,52,48,61,58,72,69,75,68,82,90,95]
 
 # line chart
@@ -72,4 +72,30 @@ plt.ylabel('Sales (Rs. K)')
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
 plt.show()
+
+cities = ['Bhopal', 'Indore', 'Jabalpur', 'Gwalior', 'Ujjain']
+students = [1200, 2800, 980, 850, 650]
+colors = ['#2196F3','#4CAF50','#FF9800','#9C27B0','#F44336']
+
+plt.figure(figsize=(9,5))
+bars = plt.bar(cities,students,color=colors,edgecolor ='white',linewidth = 1.5)
+plt.title('Students Enrolled per city')
+plt.ylabel('Number of Students')
+for bar, val in zip(bars,students):
+    plt.text(bar.get_x()+ bar.get_width()/2,val+30,str(val), ha = 'center', fontweight ='bold')
+plt.tight_layout()
+plt.show()'''
+
+study_hrs = np.random.uniform(2,10,50)
+marks = study_hrs * 7 + np.random.normal(0,8,50)
+marks = np.clip(marks, 30, 100)
+
+plt.figure(figsize=(8,5))
+plt.scatter(study_hrs, marks, c=marks, cmap='RdYlGn', s=100, alpha = 0.8)
+plt.colorbar(label = 'Marks')
+plt.title('Study Hours vs Exam Marks')
+plt.xlabel('study Hours/Day')
+plt.ylabel('Exam Marks')
+plt.show()
+
 
