6bc159fbdc369984770bc45785e466c76c08a9da
sbhoomika069-png <sbhoomika069@gmail.com>
2026-06-18 17:28:24 +0530

    update

--- AIML1.py
+++ AIML1.py
@@ -1,12 +1,13 @@
 '''statistics for AI/ML'''
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
 
 
 #employee salaries
-salaries = [22,28,35,42,38,55,48,60,72,85,30,45,52,65,28,34,41,58,75,90]
+'''salaries = [22,28,35,42,38,55,48,60,72,85,30,45,52,65,28,34,41,58,75,90]
 
 #central tendency - where is the 'center' of data
 mean = np.mean(salaries)
@@ -15,9 +16,9 @@ mode= stats.mode(salaries,keepdims=True).mode[0]
 
 print(f'mean (average): Rs.{mean}K')
 print(f'median (average): Rs.{median}K')
-print(f'mode (most common): Rs.{mode}K')
+print(f'mode (most common): Rs.{mode}K')'''
 
-'''spread-- how varied is data'''
+'''spread-- how varied is data
 std = np.std(salaries)
 var = np.var(salaries)
 rng = max(salaries)-min(salaries)
@@ -31,4 +32,28 @@ print(f'IQR: {iqr}K (Q1={q1}, Q3={q3})')
 lower = q1 - 1.5*iqr
 upper = q3 + 1.5*iqr
 outliers=[x for x in salaries if x<lower or x>upper ]
-print(f'outliers: {outliers}')
+print(f'outliers: {outliers}')'''
+
+#Data
+np.random.seed(42)
+study = np.random.uniform(2, 10, 60)
+marks = study * 8 + np.random.normal(0, 10, 60)
+marks = np.clip(marks, 30, 100)
+absent = 10 - study + np.random.normal(0, 1, 60)
+
+df = pd.DataFrame({'study_Hours':study, "marks":marks, "absence":absent})
+
+corr_matrix = df.corr()
+print(corr_matrix,round(3))
+
+plt.figure(figsize=(6,4))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1, fmt='.2f')
+plt.title('Correlation Matrix'); plt.show()
+
+# pearson correlation
+r, p_value = stats.pearsonr(study, marks)
+print(f'study_marks correlation: r=(r: .3f), p=(p_value: .4f)')
+print('Interpretation', 'strong positive' if r>0.7 else 'Moderate' if r>0.4 else 'weak')
+
+
+
