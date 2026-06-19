fcd91b594fa1bcf6122e2cb08d6edff36d8147c8
sbhoomika069-png <sbhoomika069@gmail.com>
2026-06-19 17:20:25 +0530

    update

--- probability1.py
+++ probability1.py
@@ -13,7 +13,7 @@ print(f'68% of people: {mean_h-std_h :.0f}cm to {mean_h+std_h: .0f}cm')
 print(f'95% of people: {mean_h-2*std_h: 0f}cm to {+2*std_h: .0f}cm')
 print(f'99.7% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h: .0f}')'''
 
-from sklearn.model_selection import train_test_split, cross_val_score
+'''from sklearn.model_selection import train_test_split, cross_val_score
 
 np.random.seed(42)
 x = np.random.randn(500, 5)
@@ -29,4 +29,28 @@ from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier(n_estimators=50 , random_state=42)
 cv_scores = cross_val_score(model, x, y, cv = 5, scoring = 'accuracy')
 print(f'cv score each fold:{cv_scores,round(3)}')
-print(f'mean: {cv_scores.mean(): .4f} = {cv_scores.std():.4f}')
+print(f'mean: {cv_scores.mean(): .4f} = {cv_scores.std():.4f}')'''
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+#data
+n_A, conv_A = 1000, 52
+n_B, conv_B = 1000, 68
+rate_A = conv_A/ n_A
+rate_B = conv_B/ n_B
+
+print(f'version A conversion rate: {rate_A*100: .1f}%')
+print(f'version B conversion rate: {rate_B*100: .1f}%')
+print(f'Improvement: {(rate_B-rate_A)/rate_A*100:.1f}%')
+
+#chi square test for statistical significant
+table = [[conv_A, n_A-conv_A],[conv_B, n_B-conv_B]]
+chi2,p_value,dof,expected =stats.chi2_contingency(table)
+
+print(f'chi-square: {chi2:.4f}')
+print(f'P_value: {p_value:.4f}')
+
+print('Result:', 'SIGNIFICANT - B is better!'if p_value<0.05 else 'Not significant -could be random')
+
